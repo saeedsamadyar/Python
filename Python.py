d04c5f851cd9c8aed9ffa762f7346b56ba26556f
myubunto <saeedsamadyar@yahoo.com>
2022-08-23 05:00:15 -0700

    Tue, 23 Aug

---
+++ Python.py
@@ -0,0 +1,27 @@
+#Python Data Type
+  #int
+  #Float
+  #bool
+  #complex
+  #tuple
+  #dict
+  #set
+  #bytes
+
+
+i = 2
+print(type(i))
+print(i)
+
+f = 4.5
+print(type(f))
+print(f)
+
+
+s = 'python'
+print(type(s))
+print(s)
+
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
