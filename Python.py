29ce4304085954313602df73909384b01c88f653
myubunto <saeedsamadyar@yahoo.com>
2022-09-03 00:04:00 -0700

    2, Sept

--- Python.py
+++ Python.py
@@ -7,21 +7,25 @@
   #dict
   ###set
   ###bytes
-##
+##--------------------------------------
 
 i = 2
 print(type(i))
 print(i)
-
+#------------------------------------
 f = 4.5
 print(type(f))
 print(f)
-
-
+#------------------------------------
+t = 'Python'
+print(type(t))
+print(t)
+#------------------------------------
 s = 'python'
 print(type(s))
 print(s)
 
+#------------------------------------
 
 thistuple = ("apple", "banana", "cherry")
 print(thistuple)
