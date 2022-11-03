880c7c126e88b441fc079369ebdb3522410ed42c
saeed s <saeedsamadyar@yahoo.com>
2022-11-03 19:35:17 +0330

    11/03/2022,Nov

---
+++ PythonForBeginer/DataTypesinPython(2).py
@@ -0,0 +1,10 @@
+#DataTypes
+
+print(type('James'))     #This is a string
+print('-------------------------')
+print(type(23j))         #This is a complex
+print('-------------------------')
+print(type(77.6))        #This is a float
+print('-------------------------')
+print(type(189))         #This is a integer
+
---
+++ PythonForBeginer/OperatorsInPython(3).py
@@ -0,0 +1,11 @@
+#Operators
+
+print(4 + 6)
+print(5 + 10 - 5)
+print(4 * 10 * 30)
+print( 10 / 3)
+print(1 + 12 * 2 / 4)
+print(8 % 3)
+print(4 ** 2)
+print(8 // 3)
+print(type(7 // 2))
