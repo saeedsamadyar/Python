6cfd1ea916e867d2848710540208b26f59014e6c
saeed s <saeedsamadyar@yahoo.com>
2022-11-03 19:17:55 +0330

    11/03/2022,Nov

--- DataTypesinPython.py
+++ DataTypesinPython.py
@@ -1,7 +1,10 @@
 #DataTypes
 
 print(type('James'))
+print('-------------------------')
 print(type(23j))
+print('-------------------------')
 print(type(77.6))
+print('-------------------------')
 print(type(189))
 
