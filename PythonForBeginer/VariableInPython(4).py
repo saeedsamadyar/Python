ed4ff9bbd5a854218faa8704d67c9cad2adf36b6
saeed s <saeedsamadyar@yahoo.com>
2022-11-03 20:11:50 +0330

    11/03/2022,Nov

---
+++ PythonForBeginer/VariableInPython(4).py
@@ -0,0 +1,20 @@
+#Variables
+
+a = 23
+b = 12
+print(a + b)
+# ---------------------------------------
+name = 'James'
+print(name)
+# ----------------------------------------
+x = 10
+print(x)
+
+x = 34
+print(x)
+# ----------------------------------------
+myFirstName  = 'james'                 #CamelCase
+print(myFirstName)
+# ------------------------------------------
+url  = 'https://google.com'
+print(url)
