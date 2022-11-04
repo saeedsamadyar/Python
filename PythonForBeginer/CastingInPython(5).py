5c7a5c7a3c23ba32eff02c90a6ced843a1a40b05
saeed s <saeedsamadyar@yahoo.com>
2022-11-04 22:39:11 +0330

    Nov 4th, 2022

--- PythonForBeginer/CastingInPython(5).py
+++ PythonForBeginer/CastingInPython(5).py
@@ -3,3 +3,20 @@
 x = int(4.6)
 print(type(x))
 print(x)
+
+print('--------------------------')
+
+y = float(8)
+print(type(y))
+print(y)
+
+print('---------------------------')
+
+z = float(45)
+print(type(z))
+print(z)
+
+print('----------------------------')
+X = str(30)
+print(type(X))
+print(X)
---
+++ PythonForBeginer/StringsInpython(6).py
@@ -0,0 +1,55 @@
+#Strings
+
+name = 'william'
+familyname = 'boocker'
+print('william','booker')
+
+print('---------------------------')
+
+my_name = ('my_name is: '"Johne")
+print(my_name)
+
+print('--------------------------')
+x = 'I am John\nbooker'
+print(x)
+
+print('--------------------------')
+my_name = "my name is:", "John boker"
+print(my_name)
+print("---------------------------")
+
+# SlicingStrings
+
+x = 'most likely'
+print(x[2])                #index
+print(x[5])
+print(x[2:7])
+print(x[1:11])
+print(x[1:])
+print(x[:11])
+print(x[-2:-5])
+
+print('----------------------------')
+
+# String Concatenate
+
+name = 'John'
+familyname =  'Booker'
+city = 'NYC'
+# me = name + " " + familyname + " " + city
+me = f'{name} {familyname} {city}'              # format String
+print(me)
+
+print('----------------------------')
+
+age = '33'
+# txt = "my age is: " + str(age)
+city = 'nyc'
+txt = f'{age} {city}'                           #format string
+print(txt)
+
+print('---------------------------')
+
+
+
+
