0a06bdf6ba988f54b489590b89705e1999a906eb
myubunto <saeedsamadyar@yahoo.com>
2022-11-05 03:34:38 -0700

    New Update

---
+++ PythonForBeginer/BooleansInPython(7).py
@@ -0,0 +1,21 @@
+# Booleans
+
+# print(bool("w3school"))
+# print(bool(20))
+# print(bool(false))
+# print(bool(none))
+# print(bool(a))
+# print(bool(""))
+
+x = 5
+y = 5
+# print(x == y)
+# print(x !+ y)
+# print(x > y)
+# print(x < y)
+# print(x >= y)
+print(x > y)
+
+
+
+
