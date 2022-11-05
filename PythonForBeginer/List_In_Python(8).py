a4d47283174e410e630e3acbfe217099465807c6
myubunto <saeedsamadyar@yahoo.com>
2022-11-05 06:10:21 -0700

    New Update

---
+++ PythonForBeginer/List_In_Python(8).py
@@ -0,0 +1,20 @@
+#list
+
+my_list = ['apple','orange','banana']
+print(my_list)
+
+print ('-----------------------------')
+
+mylist = ['John','william','Kris','Sarah',3,True]
+numbers = [1,2,3,4]
+print(type(mylist))
+print(mylist)
+print(mylist[2])
+print(len(mylist))
+print(numbers)
+
+print('-------------------------------')
+
+name = "John Booker"
+print(len(name))
+
