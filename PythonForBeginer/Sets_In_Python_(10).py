d750d974fee3149c3f187c7d4e264c010ab91a98
myubunto <saeedsamadyar@yahoo.com>
2022-11-07 01:57:18 -0800

    New Update

---
+++ PythonForBeginer/Sets_In_Python_(10).py
@@ -0,0 +1,13 @@
+#Sets
+
+sets = {'apple','banana','orange','cherry'}
+print(type(sets))
+print(sets)
+
+print('---------------------------')
+
+myset = {'william','John','Saeed','Booker',''}
+print('william' in myset)
+myset.add("James")
+print(myset)
+
