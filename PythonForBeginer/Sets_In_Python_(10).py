2d94651f6a6ce0e2feb47ab777ba6f3e3b8f92e0
myubunto <saeedsamadyar@yahoo.com>
2022-11-07 05:28:27 -0800

    New Update

--- PythonForBeginer/Sets_In_Python_(10).py
+++ PythonForBeginer/Sets_In_Python_(10).py
@@ -28,6 +28,21 @@ set3 = set2.union(set)
 #set = set.update(set2)
 print(set3)
 
+print('-----------------------------')
+
+my_set = {"a","b","c","d","IBM","apple"}
+my_set2 = {"Microsoft","IBM","apple"}
+my_set3 = my_set.intersection_update(my_set)              #update
+my_set.symmetric_difference_update(my_set2)
+print(my_set3)
+
+print('-----------------------------')
+
+my_set = {"a","b","c","d","IBM","apple"}
+my_set1 = {"Microsoft","IBM","apple"}
+print(my_set | my_set1)
+
+
 
 
 
