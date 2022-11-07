896b456c39355f5dbaff7b7a87d5d1647869b31b
myubunto <saeedsamadyar@yahoo.com>
2022-11-07 03:14:33 -0800

    New Update

--- PythonForBeginer/Sets_In_Python_(10).py
+++ PythonForBeginer/Sets_In_Python_(10).py
@@ -14,11 +14,21 @@ print('william' in myset)                      #show True or False  (in)
 myset.add("James")        #use method
 myset.update(myset2)
 myset.discard('John')         #remove item of list
-myset3 = myset.union(myset2)
+#myset3 = myset.union(myset2)
 #myset.pop()
 #myset.clear()
 #del myset                    #delete variable
-print(myset3)
+print(myset)
+
+print('-----------------------------')
+set = {1,2,3,4}
+set2 = {'a','b','c','d'}
+set3 = set2.union(set)
+print(set3)
+
+
+
+
 
 
 
