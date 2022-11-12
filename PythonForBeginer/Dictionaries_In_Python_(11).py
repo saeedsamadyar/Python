c2a501277a31e4682df355a8b3c699dae82cd72f
myubunto <saeedsamadyar@yahoo.com>
2022-11-12 03:02:15 -0800

    New Update

--- PythonForBeginer/Dictionaries_In_Python_(11).py
+++ PythonForBeginer/Dictionaries_In_Python_(11).py
@@ -1,6 +1,6 @@
 # Dictionaries                     ()Tuples          [] List         {} Set
 # --------------------------------
-
+# first list
 me = {
     "name": "John",
     "lastname": "Booker",
@@ -9,6 +9,14 @@ me = {
     "friends": ['william', 'john', 'philip'],  # list
     "Happy": False  # boolian
 }
+# second list
+me2 = {
+    "name": "saeed",
+    "lastname": "s",
+    "age": 35,
+    "myfriend": ['mojan', 'depi', 'willia', 'jessi']
+}
+
 print(type(me))
 print(len(me))
 print(me)
@@ -37,40 +45,52 @@ print(me)
 
 print('-----------------------------')
 
-x = me.values()             #value method
+x = me.values()  # value method
 print(x)
 
 print('----------------------------')
 
-y = me.items()              #items method
+y = me.items()  # items method
 print(y)
 
 print('----------------------------')
 
-x = "name" in me           #boolian
+x = "name" in me  # boolian
 print(x)
 
 print('----------------------------')
 
-me["age"] = 47             # change value in key
-me['Friend'] = "Julia"     # Add key and value in dictionary
-me.update({"age": 98})     # change value in key
+me["age"] = 47  # change value in key
+me['Friend'] = "Julia"  # Add key and value in dictionary
+me.update({"age": 98})  # change value in key
 print(me)
 
 print('-----------------------------')
 
-#me.pop('name')              # remove key
+# me.pop('name')              # remove key
 print(me)
 
 print('--------------------------------')
 
-me.popitem()                 # remove last item
+me.popitem()  # remove last item
 print(me)
 
 print('--------------------------------')
 
-del me['name']              #remove key
+del me['name']  # remove key
 print(me)
 
-me.clear()
+me.clear()  # clear dictionaries
+print(me)
+
+print('--------------------------------')
+
+# me2 = me.copy()    # copy
+# me2['friend'] = 'jack'
+# print(me)
+
+print('---------------------------------')
+
+me = dict(me2)          # dict is a function is doing copy of list
+me['colors'] = 'red'
 print(me)
