d666b6c2fb08625ae8772e091766f435f1bc606d
myubunto <saeedsamadyar@yahoo.com>
2022-11-15 03:07:05 -0800

    New Update

--- PythonForBeginer/Rehearsal/login (2).py
+++ PythonForBeginer/Rehearsal/login (2).py
@@ -5,5 +5,8 @@ entered_password = input('please enter your password: ')
 while entered_password != stored_password:
     entered_password = input('you are wrong please enter again: ')
 
+    continue
+
+if entered_password == stored_password:
     print('you logged successfully')
 
