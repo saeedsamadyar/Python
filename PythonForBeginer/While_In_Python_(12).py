9a3cafd86ed3aa160b8d34be8fe136a70e905bbf
myubunto <saeedsamadyar@yahoo.com>
2022-11-14 23:37:29 -0800

    New Update

--- PythonForBeginer/While_In_Python_(12).py
+++ PythonForBeginer/While_In_Python_(12).py
@@ -6,14 +6,25 @@
 
 print('------------------------')
 
-num = 1
-while num < 6:
-    print(num)
-    num = num + 1
+# num = 1
+# while num < 6:
+#     print(num)
+#     num = num + 1
 
 print('-------------------------')
 
-num1 = 2
-while num1 > 1:
-    print(num1)
-    num1 = + 1
+# num1 = 2
+# while num1 > 1:
+#     print(num1)
+#     num1 = + 1
+
+print('--------------------------')
+
+num3 = 1
+while num3 < 10:
+    num3 += 1
+    if num3 == 10:
+        break
+    print(num3)
+
+
