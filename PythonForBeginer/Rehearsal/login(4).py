e11bdb2caea3eddeba9f53861125bac046dd1997
saeed s <saeedsamadyar@yahoo.com>
2022-11-17 23:39:18 +0330

    New Update

---
+++ PythonForBeginer/Rehearsal/login(4).py
@@ -0,0 +1,21 @@
+users = {  # dictionary
+    "william": "123",
+    "john": "321",
+    "jessi": "456",
+}
+
+entered_username = input('Please enter your username: ')
+entered_password = input('Please enter your password: ')
+
+while entered_username in users and users[entered_username] == entered_password:
+    print('congrats, you are our user')
+
+    break
+
+else:
+
+    if entered_username in users and users[entered_username] != entered_password:
+        print('sorry, you are not our user, Try again')
+
+
+
