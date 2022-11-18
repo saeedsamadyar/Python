196b6589e5750b50b9a9640331d892348258ade7
saeed s <saeedsamadyar@yahoo.com>
2022-11-18 11:32:08 +0330

    New update

--- PythonForBeginer/For_loop_In_Python(13).py
+++ PythonForBeginer/For_loop_In_Python(13).py
@@ -1,8 +1,25 @@
-#For loop
+# For loop
+#
+# friends = ['william', 'john', 'jessi', 'booker']  # list
+#
+# for items in friends:
+#     print('hi')
+#     print(items)
+#     print('baayy')
 
-friends = ['william','john','jessi','booker']         # list
+print('-------------------------------')
 
-for items in friends:
-    print('hi')
-    print(items)
-    print('baayy')
+# name = ["Jessi"]
+#
+# for x in name:
+#     print(x)
+
+print('-------------------------------')
+
+my_family = ['John', 'william', 'philip', 'jessi']
+
+for item in my_family:
+    if item == 'jessi':
+
+
+        print(item)
