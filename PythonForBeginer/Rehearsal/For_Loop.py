bae49b20212a3a07bc78e7f8ffc00e62718a2399
myubunto <saeedsamadyar@yahoo.com>
2022-11-19 04:01:41 -0800

    New Update

--- PythonForBeginer/Rehearsal/For_Loop.py
+++ PythonForBeginer/Rehearsal/For_Loop.py
@@ -4,8 +4,8 @@ names = ['william','john','jessi','rose','max','julia']
 b = []
 
 for name in names:
-    if name[0] == "r":
+    if name[0] == "j":
         b.append(name)
 
         print(b)
-        print(type(b))
+        print(len(b))
