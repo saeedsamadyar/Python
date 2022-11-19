0e8e3c96c67c1ccef86528b6707f8a96c6497ce0
myubunto <saeedsamadyar@yahoo.com>
2022-11-19 04:18:23 -0800

    New Update

---
+++ PythonForBeginer/Rehearsal/For_loop_(1).py
@@ -0,0 +1,12 @@
+
+a = ['william','julia','jessi','rose']
+b = ['max','maryam','julia','jila']
+c = []
+
+for i in a:
+    for n in b:
+        if i == n:
+            c.append(i)
+
+            print(c)
+
