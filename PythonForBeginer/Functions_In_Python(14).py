79d675b77037bf420ba01a576cceb4b3f91ad59f
myubunto <saeedsamadyar@yahoo.com>
2022-11-20 03:00:26 -0800

    New Update

--- PythonForBeginer/Functions_In_Python(14).py
+++ PythonForBeginer/Functions_In_Python(14).py
@@ -1,4 +1,16 @@
-def me():
-    print('Im from United states, loving programming, loving python')
+# def me():
+#     print('Im from United states, loving programming, loving python')
+#
+# me()
 
-me()
+# print('----------------------------------')
+
+def my_name(name):
+    print(f"hello {name}")
+    print(f"how are you? {name}?")
+    print(f"please give me your info? {name}")
+
+
+    my_name("John")
+
+
