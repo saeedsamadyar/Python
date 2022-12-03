18a3463741305a1d51d0ffd4bad4ce138907ad84
saeed s <saeedsamadyar@yahoo.com>
2022-12-03 13:21:43 +0330

    New update

--- PythonForBeginer/Functions_In_Python(14).py
+++ PythonForBeginer/Functions_In_Python(14).py
@@ -1,25 +1,23 @@
-def me():
-    print('Im from United states, loving programming, loving python')
-
-me()
-
-print('----------------------------------')
-
-def my_name(name):
-    print(f"hello {name}")
-    print(f"how are you? {name}?")
-    print(f"please give me your info? {name}")
+# def me():
+#     print('Im from United states, loving programming, loving python')
+#
+# me()
+#
+# print('----------------------------------')
+#
+def my_name(name, lastname):
+    print(f"hello {name}{lastname}")
+    print(f"how are you? {name}{lastname}?")
+    print(f"please give me your info? {name}{lastname}")
 
     my_name("John", "booker")
+#
+# print("-----------------------------------")
 
-print("-----------------------------------")
-
-def sum(a,b):
-    print(a + b)
-
-    sum(2, 4)
-
-print("--------------------------------")
-
-
-
+# def sum(a, b):
+#     print(a + b)
+#
+#     sum(2, 4)
+#
+#
+# print("--------------------------------")
