a5982e900ef6957960e936abea47c20ebf467249
saeed s <saeedsamadyar@yahoo.com>
2022-12-05 20:37:56 +0330

    New update

--- PythonForBeginer/Functions_In_Python(14).py
+++ PythonForBeginer/Functions_In_Python(14).py
@@ -1,29 +1,38 @@
-# def me():
-#     print('Im from United states, loving programming, loving python')
-#
-#
-# me()
-#
-# print('----------------------------------')
-#
-# def my_name(name, lastname):
-#     print(f"hello {name}{lastname}")
-#     print(f"how are you? {name}{lastname}?")
-#     print(f"please give me your info? {name}{lastname}")
-#
-#     my_name(name="John", lastname="booker")
+def me():
+    print('Im from United states, loving programming, loving python')
 
-# print("-----------------------------------")
-#
-# def sum(a,b):
-#     print(a + b)
-#
-#     sum(a = 2,b = 4)
-#
-# print("--------------------------------")
+
+me()
+
+print('----------------------------------')
+
+def my_name(name, lastname):
+    print(f"hello {name}{lastname}")
+    print(f"how are you? {name}{lastname}?")
+    print(f"please give me your info? {name}{lastname}")
+
+    my_name(name="John", lastname="booker")
+
+print("-----------------------------------")
+
+def sum(a,b):
+    print(a + b)
+
+    sum(a = 2,b = 4)
+
+print("--------------------------------")
 
 def fullname(name, lastname):
     print(f"hello {name} {lastname}")
 
     fullname(name = "john", lastname = "willi")
+
+
+print("---------------------------------")
+
+def callname(fname, lname):
+    print(f"{fname} + " " + {lname}" )
+
+    callname(fname = "jessi", lname = "pinkman")
+
 
