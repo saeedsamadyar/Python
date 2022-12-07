9cdc09f9f39db287eb538a9246614ddcb188517f
saeed s <saeedsamadyar@yahoo.com>
2022-12-07 14:50:20 +0330

    new update

--- PythonForBeginer/Functions_In_Python(14).py
+++ PythonForBeginer/Functions_In_Python(14).py
@@ -1,98 +1,114 @@
-def me():
-    print('Im from United states, loving programming, loving python')
+# def me():
+#     print('Im from United states, loving programming, loving python')
+#
+#
+# me()
+#
+# print('----------------------------------')
+#
+#
+# def my_name(name, lastname):
+#     print(f"hello {name}{lastname}")
+#     print(f"how are you? {name}{lastname}?")
+#     print(f"please give me your info? {name}{lastname}")
+#
+#     my_name(name="John", lastname="booker")
+#
+#
+# print("-----------------------------------")
+#
+#
+# def sum(a, b):  # parametre
+#     print(a + b)
+#
+#     sum(a=2, b=4)  # argumant
+#
+#
+# print("--------------------------------")
+#
+#
+# def fullname(name, lastname):
+#     print(f"hello {name} {lastname}")
+#
+#     fullname(name="john", lastname="willi")
+#
+#
+# print("---------------------------------")
+#
+#
+# def callname(fname, lname):
+#     print(f"{fname} +""+ {lname}")
+#
+#     callname(fname="jessi", lname="pinkman")
+#
+#
+# print("--------------------------------")
+#
+#
+# def callname(fname, lname, *args):  # *arges means tuples
+#     print(f"hello{fname}{lname}")
+#     callname("jessi", "pinkman", "willlii")
+#
+#     print("----------------------------")
+#
+#
+# def hello(*names):  # *arges Tuples
+#     for name in names:
+#         print(f"hello{name}")
+#
+#         hello("john", "willi", "jessi")
+#
+#
+# print("------------------------------")
+#
+#
+# def hello(lname, fname, *name):  # *argesTuples
+#     print(fname)
+#     print(lname)
+#     print(name)  # *arges
+#
+#
+# hello("john", "jessi", "alia", "jessi", "julia")
+#
+# print('---------------------------')
+#
+#
+# def myname(lname, fname, *args, **kwargs):
+#     print(lname)
+#     print(fname)
+#     print(args)  # Tuples
+#     print(kwargs)  # Dictionary
+#
+#     myname("booker", "jessi", "will", "john", age=33, city="nyc", Team="tajbibi")
+#
+#
+# print('-----------------------------')
+#
+#
+# def name(fname="jessi", lname="booker"):
+#     print(f"my fullname is: {fname}{lname}")
+#
+#     name()
+#
+#
+# print('-----------------------------')
+#
+#
+# def name(a):
+#     for names in a:
+#         print(f"my name is:{names}")
+#
+#         name("jessi")
+#
+#
+# print("-----------------------------")
+#
 
+def sum(a, b):
+    a += 1
+    b += 3
 
-me()
-
-print('----------------------------------')
-
-
-def my_name(name, lastname):
-    print(f"hello {name}{lastname}")
-    print(f"how are you? {name}{lastname}?")
-    print(f"please give me your info? {name}{lastname}")
-
-    my_name(name="John", lastname="booker")
-
-
-print("-----------------------------------")
-
-
-def sum(a, b):  # parametre
-    print(a + b)
-
-    sum(a=2, b=4)  # argumant
-
-
-print("--------------------------------")
-
-
-def fullname(name, lastname):
-    print(f"hello {name} {lastname}")
-
-    fullname(name="john", lastname="willi")
-
-
-print("---------------------------------")
-
-
-def callname(fname, lname):
-    print(f"{fname} +""+ {lname}")
-
-    callname(fname="jessi", lname="pinkman")
-
-
-print("--------------------------------")
-
-
-def callname(fname, lname, *args):  # *arges means tuples
-    print(f"hello{fname}{lname}")
-    callname("jessi", "pinkman", "willlii")
-
-    print("----------------------------")
-
-
-def hello(*names):  # *arges Tuples
-    for name in names:
-        print(f"hello{name}")
-
-        hello("john", "willi", "jessi")
-
-
-print("------------------------------")
-
-
-def hello(lname, fname, *name):  # *argesTuples
-    print(fname)
-    print(lname)
-    print(name)  # *arges
-
-
-hello("john", "jessi", "alia", "jessi", "julia")
-
-print('---------------------------')
-
-
-def myname(lname, fname, *args, **kwargs):
-    print(lname)
-    print(fname)
-    print(args)      #Tuples
-    print(kwargs)    #Dictionary
-
-    myname("booker", "jessi", "will", "john", age=33, city="nyc", Team="tajbibi")
-
-print('-----------------------------')
-
-def name(fname="jessi",lname="booker"):
-    print(f"my fullname is: {fname}{lname}")
-
-    name()
-
-
-print('-----------------------------')
-
-def name(a):
-    for names in a:
-        print(f"my name is:{names}")
-
-        name("jessi")
+    return (a, b)
+result = sum(3, 5)
+for item in result:
+    print(item)
