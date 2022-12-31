09bf113fe58cb76aea43f1dc0ccf31b939621902
saeed s <saeedsamadyar@yahoo.com>
2023-01-01 01:08:18 +0330

    new update

--- PythonForBeginer/Rehearsal/Function.py
+++ PythonForBeginer/Rehearsal/Function.py
@@ -1,19 +1,30 @@
-def func(name):
-    ups = 0
-    lows = 0
-    for item in name:
-        if item.isupper():  # method
-            ups += 1
-        elif item.islower():  # method
-            lows += 1
+# def func(name):
+#     ups = 0
+#     lows = 0
+#     for item in name:
+#         if item.isupper():  # method
+#             ups += 1
+#         elif item.islower():  # method
+#             lows += 1
+#     else:
+#         pass
+#
+#     print(f"upper Case is:{ups}")
+#     print(f"lower Case is:{lows}")
+#
+#
+# while True:
+#     name = input("Please enter your name: ")
+#     func(name)
+#
+# print('-------------------------------')
+
+def numbertype(number):
+    if number % 2 == 0:
+        print("This is an even number")
     else:
-        pass
-
-    print(f"upper Case is:{ups}")
-    print(f"lower Case is:{lows}")
-
+        print("This is an odd number")
 
 while True:
-    name = input("Please enter your name: ")
-    func(name)
-
+    number = int(input("Please enter a number: "))
+    numbertype(number)
