0a1515b79580fee3ce694c4ee124db504aa10da5
saeed s <saeedsamadyar@yahoo.com>
2022-12-31 21:23:31 +0330

    new update

---
+++ PythonForBeginer/Rehearsal/Function.py
@@ -0,0 +1,18 @@
+def func(name):
+    ups = 0
+    lows = 0
+    for item in name:
+        if item.isupper():  # method
+            ups += 1
+        elif item.islower():  # method
+            lows += 1
+        else:
+
+            pass
+        print(f"upper Case is:{ups}")
+        print(f"lower Case is:{lows}")
+
+
+name = input("Please enter your name:")
+
+func(name)
