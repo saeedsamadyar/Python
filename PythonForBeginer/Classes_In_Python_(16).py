c78e476454a650f2f13f6e8d531f95bedfc5e876
saeed s <saeedsamadyar@yahoo.com>
2023-01-07 01:22:25 +0330

    new update

--- PythonForBeginer/Classes_In_Python_(16).py
+++ PythonForBeginer/Classes_In_Python_(16).py
@@ -1,11 +1,24 @@
+# class MyClass:
+#     x = 2  # Prpperty
+#     y = 4  # property
+#
+#
+# p1 = MyClass()  # Objest1
+# p2 = MyClass()  # Object2
+#
+# print(p1.x)
+# print(p2.y)
+
+print('------------------------')
+
+
 class MyClass:
 
+    def __int__(self, name, lastname):
+        self.myname = name
+        self.mylastname = lastname
 
- x = 2  # Prpperty
- y = 4  # property
+        p1 = MyClass("John", "pinkman")
 
-p1 = MyClass()  # Objest1
-p2 = MyClass()  # Object2
-
-print(p1.x)
-print(p2.y)
+        print(p1.myname)
+        print(p1.mylastname)
---
+++ PythonForBeginer/Rehearsal/class_def_init.py
@@ -0,0 +1,11 @@
+
+class MyClass:
+
+    def __int__(self, name, lastname):
+        self.myname = name
+        self.mylastname = lastname
+
+        p1 = MyClass ("john","wich")
+
+        print(p1.myname)
+        print(p1.mylastname)
