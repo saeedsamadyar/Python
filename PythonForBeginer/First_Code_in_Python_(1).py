7bac6e0d4c7b73dc2ec3667772c0f46a16ea215e
saeed s <saeedsamadyar@yahoo.com>
2023-04-25 19:55:19 +0400

    newupdate

--- PythonForBeginer/First_Code_in_Python_(1).py
+++ PythonForBeginer/First_Code_in_Python_(1).py
@@ -1,3 +1,25 @@
 #FirstCode
 
 print('Hello, \n World.!', sep = "!", end = "?")
+print('2 % 6')
+
+x = 10 / 4
+y = 5 / 2.0
+print (x + y)
+
+print(13 / 4 + 13 % 4)
+
+print(2 * 3 ** 3 * 4)
+
+print (6. // 4)
+
+print (10 - 6 ** 2 / 9 * 10 + 1)
+
+print(2 ** 3.)
+
+age = 22
+AGE = 44
+age /= 2
+print(age + AGE)
+
+print(int(15.5)-10)
